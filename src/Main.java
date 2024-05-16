87c52f2a7b70d691fe3d30556f2b07a2b1603521
Mazen Atef <mazenatef5510@gmail.com>
2024-05-16 17:22:05 +0300

    New changes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,20 @@
-public class Main {
-    public static void main(String[] args) {}
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public Parent root;
+    public Scene currScene;
+    public Stage currStage;
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //draw
+    }
 }
